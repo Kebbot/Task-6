ab6541068a489fc536631da4ee33c82c293345fc
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-02-20 22:37:09 +0300

    Task-6

---
+++ Task/Task.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float s, t, v;
+
+	cout << "Введите расстояние до Аэропорта (в км): ";
+	cin >> s;
+	cout << "\nВведите время за которое надо доехать (в мин): ";
+	cin >> t;
+	v = s / t;
+	cout << "\nСкорость с которой надо ехать: ";
+	cout << v << " Км/мин.";
+
+
+	return 0;
+
+}
